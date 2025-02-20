5f55cd12142f15b22aa6824c9100ae5a797e1ef2
jane7353n0 <jane7353n0@gmx.de>
2025-02-20 19:56:21 +0100

    Update get-spell.mjs

--- netlify/functions/get-spell/get-spell.mjs
+++ netlify/functions/get-spell/get-spell.mjs
@@ -1,5 +1,5 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
-import { MongoClient } from "./node_modules/mongodb";
+import { MongoClient } from "mongodb";
 
 const mongoClient = new MongoClient(process.env.MONGODB_URI);
 const clientPromise = mongoClient.connect();
