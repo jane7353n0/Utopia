d1e4569e7dd607492310fd71b5ad447a994dab0a
jane7353n0 <jane7353n0@gmx.de>
2025-02-20 18:41:41 +0100

    Test netlify functions + connection to MongoDB

--- netlify/functions/get-spell/get-spell.mjs
+++ netlify/functions/get-spell/get-spell.mjs
@@ -1,4 +1,28 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const clientPromise = mongoClient.connect();
+
+const db = "";
+const col = "";
+
+const handler = async (event) => {
+  try {
+    const database = (await clientPromise).db(db);
+    const collection = database.collection(col);
+    const results = await collection.find({}).limit(10).toArray();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
+/*
 export default (request, context) => {
   try {
     const url = new URL(request.url)
@@ -11,3 +35,4 @@ export default (request, context) => {
     })
   }
 }
+*/
